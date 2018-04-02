7fa827fdccdbb6acca1f5667165ed3a708a3e956
grapefruit95 <rocco.perciavalleiii@gmail.com>
2018-04-01 20:59:19 -0500

    adding php experimentation

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+		<title>Rocco Perciavalle's project site</title>
+	</head>
+	<body>
+        <h1>Rocco Perciavalle's project site</h1>
+        <a href='flaggame.html'>Python Flag Game</a>
+        <?php
+        for($x = 0; $x = 100; $x = $x + 10):
+        echo $x;
+        endfor;
+        ?>
+	</body>
+</html>
